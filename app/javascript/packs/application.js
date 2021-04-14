c85b0c3c897cdf304b45b9576461a018e2a9d24f
Davids Chijioke Prince <davids.chijioke@gmail.com>
2021-04-14 02:30:01 +0100

    Add semantic ui and favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// reqiure jquery, 
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -10,4 +12,13 @@ import "channels"
 
 Rails.start()
 Turbolinks.start()
+require("semantic-ui-sass")
 ActiveStorage.start()
+
+$ (document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown()
+;
+})
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'chatroom#index'
-  get 'login', to: 'sessions#new'
+  get 'new', to: 'sessions#new'
+  # get 'login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
