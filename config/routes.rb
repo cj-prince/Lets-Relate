486ffdc154391b2ed410a51c3e020c6391e8b35d
Davids Chijioke Prince <davids.chijioke@gmail.com>
2021-04-19 10:43:44 +0100

    Generate chatroom channel and add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'chatroom#index'
   resources :messages
+
+
+  mount ActionCable.server, at: '/cable'
   # post 'messages', to: 'messages#create'
   # get 'login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
